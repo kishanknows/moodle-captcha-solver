62c2b389bdff6215459bcb2c8c46ba891a4c3ca6
Kishan <kishanknows@gmail.com>
2023-01-16 16:33:32 +0530

    created scripts

---
+++ scripts.js
@@ -0,0 +1,16 @@
+console.log("hello");
+console.log(document.getElementById("valuepkg3").value);
+const str = document.getElementById("login").innerText.split("\n")[3];
+const nums = str.match(/(\d+)/g);
+var ans = 0;
+if (str.match("add")) {
+  ans = +nums[0] + +nums[1];
+  document.getElementById("valuepkg3").value = ans.toString();
+} else if (str.match("subtract")) {
+  ans = +nums[0] - +nums[1];
+  document.getElementById("valuepkg3").value = ans.toString();
+} else if (str.match("first")) {
+  document.getElementById("valuepkg3").value = nums[0];
+} else if (str.match("second")) {
+  document.getElementById("valuepkg3").value = nums[1];
+}
